556c5a65d197ed3d5c584b9b5b468c4b23d6cd8a
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-01-27 16:57:58 +0100

    blueprint arr done

--- board.js
+++ board.js
@@ -13,7 +13,20 @@ class Board {
             //way: null,
         }
 
-        this.lvlBluePrint = 
+        this.lvlBluePrint = [
+            [1, 3, 1, 1, null, 1, null, null, 1, null, 3, 1],
+            [1, 4, 1, null, null, null, 3, null, 3, null, null, null,],
+            [3, 3, 3, 3,  null, 1, 3, 1,  null, 1,  null, 1],
+            [1, null, 1, 1, null, null, 1, 1, 1, 3, 3, 3],
+            [3, null, null, 3, null, null, 1, null, 1, null, null, null],
+            [3, null, 1, null, null, null, 1, 3, 3, null, 1, 1],
+            [3, null, 1, null, null, 1, 3, null, null, null, 3, null],
+            [1, 1, null, 3, null, 1, 3, null, null, 3, null, null],
+            [3, 3, null, null, null, 1, 1, 1, 1, 1, null, 1],
+            [null, 1, null, 1, 1, 1, 3, 3, 3, null, 3, 3],
+            [3, 3, null, 3, 3, null, 3, 3, 3, 3, null, null],
+            [3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 1, 2],
+        ];
 
     }
 }
